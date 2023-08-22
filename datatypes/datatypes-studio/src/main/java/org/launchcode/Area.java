676740188cdc6032cd9787324b996c7bd10f8ace
rama <rumneel@gmail.com>
2023-08-21 20:17:07 -0500

    created two files for radius and area of circle

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        Double radius = input.nextDouble();
+//        Double pi = 3.14;
+//        Double a = pi*radius*radius;
+
+        System.out.println("The area of a circle of radius " + radius + " is " +Circle.getArea(radius));
+
+        input.close();
+
+    }
+}
